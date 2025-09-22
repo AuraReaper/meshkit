b77011368ccde9739868e28abe1ffb941367fe79
Yash Kumar Gupta <yashkr104@gmail.com>
2025-09-22 22:26:44 +0530

    fix(validator): Resolve schema reference error in tests
    Signed-off-by: Yash Kumar Gupta <yashkr104@gmail.com>

--- validator/validate_test.go
+++ validator/validate_test.go
@@ -66,6 +66,7 @@ func TestValidator(t *testing.T) {
 			} else if !test.ShouldPass && err == nil {
 				t.Errorf("test failed for %s, got %t, want %t error: %v", test.Path, true, !test.ShouldPass, err)
 			}
+
 		})
 	}
 }
